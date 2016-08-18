0cccd13fba221bd607e25055f5c06a26d54120e4
Cynthia Bathgate <c.bathgate1@gmail.com>
2016-08-17 20:54:40 -0700

    update .js file to create grid by rows instead of inline block, overall more effective and less crashing

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
-var gridSize = +prompt("Enter a grid dimension", "Enter a value of 80 or less");
-while (gridSize > 80 || gridSize / gridSize != 1) {
-    gridSize = +prompt("Max value = 80", "Enter a value of 80 or less");
+var gridSize = +prompt("Enter a grid dimension", "Enter a value of 128 or less");
+while (gridSize > 128 || gridSize / gridSize != 1) {
+    gridSize = +prompt("Max value = 128", "Enter a value of 128 or less");
 }
 var containerSize = +$("#container").width();
 var boxSize = containerSize/gridSize;
-    for (i=0; i < gridSize * gridSize; i++) {
-        var newBox = $("<div class='box'></div>");
-        $("#container").append(newBox);
+    for (i=0; i < gridSize; i++) {
+        for (j=0; j < gridSize; j++){
+            var newBox = $("<div class='box'></div>");
+            $("#container").append(newBox);
+        }
     }
     $(".box").width(boxSize).height(boxSize);
     $(".box").hover(function() {
